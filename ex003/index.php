0e92c5d1242a4f4181e249c42b4c5a74b4cbaef4
felipekauan1 <felipekauan30@outlook.com>
2026-01-10 11:36:20 -0300

    feat: adiciona ex003 com tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex003</title>
+    <style>
+        * { 
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100dvh;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tipos Primitivos em PHP</h1>
+    <h1>
+        <?php
+            $nome = "Felipe Kauãn"; // string
+            $idade = 20; // int ou integer
+            $peso = 68.9; // float ou double
+            $casado = false; // bool ou boolean 
+        ?>
+    </h1>
+</body>
+</html>
