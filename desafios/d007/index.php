c3ca902829fcc3a459dfbad8b3ac7c46f4f99f5c
felipekauan1 <felipekauan30@outlook.com>
2026-01-13 17:37:00 -0300

    feat: adiciona d007: salário mínimo

---
+++ desafios/d007/index.php
@@ -0,0 +1,37 @@
+<?php
+    $salario_min = 1621;
+    $salario = $_GET["sala"] ?? 3300;
+
+?><!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>d007</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <main>
+        <h1>Informe seu salário</h1>
+        <form action="<?= $_SERVER["PHP_SELF"] ?>">
+            <label for="sala">Salário R$</label>
+            <input type="number" name="sala" id="sala" value="<?= $salario ?>" step="0.01" required>
+
+            <p>Considerando o salário mínimo de <strong>R$ 1.621,00</strong></p>
+
+            <input type="submit" value="Calcular">
+        </section>
+    </main>
+
+    <section>
+        <h1>Resultado Final</h1>
+        <?php
+            $int = intdiv($salario, $salario_min);
+
+            $resto = $salario % $salario_min;
+
+            echo "<p>Quem recebe um salário de R$ " .  number_format($salario, 2, ",", ".") . " ganha " . $int . " Salários Mínimos + R$ " . number_format($resto, 2, ",", ".") . "</p>";
+        ?>
+    </section>
+</body>
+</html>
